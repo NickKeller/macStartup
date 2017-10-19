9aa39bbaba2398889ef62a5f522caaead88af336
Nick Keller <nick.keller57@gmail.com>
2017-10-18 23:26:41 -0400

    added showhidden and dock popup timer alterations

---
+++ startup.sh
@@ -0,0 +1,4 @@
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Finder
+defaults write com.apple.Dock showhidden -bool TRUE; killall Dock
+defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
+
