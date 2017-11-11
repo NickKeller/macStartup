58491df2c4566a0d2578361f93e62e4861d59cd5
Nick Keller <nick.keller57@gmail.com>
2017-11-11 10:42:06 -0500

    added descriptive comments and more init commands

--- startup.sh
+++ startup.sh
@@ -1,6 +1,18 @@
+#setting defaults for Finder
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Finder
 defaults write com.apple.finder AppleShowAllFiles YES; killall Finder
 
+#Setting defaults for Dock
 defaults write com.apple.Dock showhidden -bool TRUE; killall Dock
 defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
+
+#Allowing all apps to be installed
+sudo spctl --master-disable;
+
+#installing Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#homebrew install packages
+brew update;
+brew install git kubectl htop openssh;
 
