053a0a4c5b217edc42b3ca9e3976ef779481f60b
Nick Keller <nick.keller57@gmail.com>
2017-11-11 10:21:58 -0500

    Added show all hidden files option, and some space for easier viewing

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Finder
+defaults write com.apple.finder AppleShowAllFiles YES; killall Finder
+
 defaults write com.apple.Dock showhidden -bool TRUE; killall Dock
 defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
 
