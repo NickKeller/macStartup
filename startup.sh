90343418f191968de00ad78268d42dc65ed0f780
Nick Keller <nick.keller@airbusaerial.com>
2017-11-17 12:44:58 -0500

    added delay for switching between spaces on window drag

--- startup.sh
+++ startup.sh
@@ -5,6 +5,7 @@ defaults write com.apple.finder AppleShowAllFiles YES; killall Finder
 #Setting defaults for Dock
 defaults write com.apple.Dock showhidden -bool TRUE; killall Dock
 defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
+defaults write com.apple.dock workspaces-edge-delay -float 2.0; killall Dock
 
 #Allowing all apps to be installed
 sudo spctl --master-disable;
